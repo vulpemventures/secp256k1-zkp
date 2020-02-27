c3ca3ab6866c7193c869c96a33150070373be8dd
altafan <pietro@vulpem.com>
2020-02-27 16:54:59 +0100

    Pass generator as argument from JS to pedersen_commitment C wrapper

--- lib/pedersen/index.d.ts
+++ lib/pedersen/index.d.ts
@@ -1,4 +1,8 @@
-export function commit(blindFactor: Buffer, value: string): Buffer;
+export function commit(
+  blindFactor: Buffer,
+  value: string,
+  generator: Buffer
+): Buffer;
 export function commitSerialize(commitment: Buffer): Buffer;
 export function commitParse(input: Buffer): Buffer;
 export function blindSum(blinds: Array<Buffer>, nneg?: number): Buffer;
