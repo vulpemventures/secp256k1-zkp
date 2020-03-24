a719d31a78f84d49293f680f28d6976178fac505
altafan <pietro@vulpem.com>
2020-03-24 15:29:53 +0100

    Lint & Prettify

--- test/rangeproof.js
+++ test/rangeproof.js
@@ -1,18 +1,18 @@
-const assert = require("assert");
+const assert = require('assert');
 
-const Module = require("../lib");
+const Module = require('../lib');
 const { sign, info, verify, rewind } = Module.rangeproof;
-const fixtures = require("./fixtures/rangeproof.json");
+const fixtures = require('./fixtures/rangeproof.json');
 
-describe("range proof", () => {
-  it("proof sign", () => {
-    fixtures.sign.forEach(f => {
-      const commit = Buffer.from(f.commit, "hex");
-      const nonce = Buffer.from(f.commit, "hex");
-      const blind = Buffer.from(f.blind, "hex");
-      const generator = Buffer.from(f.generator, "hex");
-      const message = Buffer.from(f.message, "hex");
-      const extraCommit = Buffer.from(f.extraCommit, "hex");
+describe('range proof', () => {
+  it('proof sign', () => {
+    fixtures.sign.forEach((f) => {
+      const commit = Buffer.from(f.commit, 'hex');
+      const nonce = Buffer.from(f.commit, 'hex');
+      const blind = Buffer.from(f.blind, 'hex');
+      const generator = Buffer.from(f.generator, 'hex');
+      const message = Buffer.from(f.message, 'hex');
+      const extraCommit = Buffer.from(f.extraCommit, 'hex');
       const proof = sign(
         commit,
         blind,
@@ -25,13 +25,13 @@ describe("range proof", () => {
         message,
         extraCommit
       );
-      assert.deepEqual(proof.toString("hex"), f.expected);
+      assert.deepEqual(proof.toString('hex'), f.expected);
     });
   });
 
-  it("proof info", () => {
-    fixtures.info.forEach(f => {
-      const proof = Buffer.from(f.proof, "hex");
+  it('proof info', () => {
+    fixtures.info.forEach((f) => {
+      const proof = Buffer.from(f.proof, 'hex');
       const proofInfo = info(proof);
       assert.deepEqual(proofInfo.exp, f.expected.exp);
       assert.deepEqual(proofInfo.mantissa, f.expected.mantissa);
@@ -40,12 +40,12 @@ describe("range proof", () => {
     });
   });
 
-  it("proof verify", () => {
-    fixtures.verify.forEach(f => {
-      const proof = Buffer.from(f.proof, "hex");
-      const commit = Buffer.from(f.commit, "hex");
-      const generator = Buffer.from(f.generator, "hex");
-      const extraCommit = Buffer.from(f.extraCommit, "hex");
+  it('proof verify', () => {
+    fixtures.verify.forEach((f) => {
+      const proof = Buffer.from(f.proof, 'hex');
+      const commit = Buffer.from(f.commit, 'hex');
+      const generator = Buffer.from(f.generator, 'hex');
+      const extraCommit = Buffer.from(f.extraCommit, 'hex');
       assert.deepEqual(
         verify(commit, proof, generator, extraCommit),
         f.expected
@@ -53,18 +53,18 @@ describe("range proof", () => {
     });
   });
 
-  it("range proof rewind", () => {
-    fixtures.rewind.forEach(f => {
-      const proof = Buffer.from(f.proof, "hex");
-      const commit = Buffer.from(f.commit, "hex");
-      const generator = Buffer.from(f.generator, "hex");
-      const extraCommit = Buffer.from(f.extraCommit, "hex");
+  it('range proof rewind', () => {
+    fixtures.rewind.forEach((f) => {
+      const proof = Buffer.from(f.proof, 'hex');
+      const commit = Buffer.from(f.commit, 'hex');
+      const generator = Buffer.from(f.generator, 'hex');
+      const extraCommit = Buffer.from(f.extraCommit, 'hex');
       const res = rewind(commit, proof, commit, generator, extraCommit);
       assert.deepEqual(res.value, f.expected.value);
       assert.deepEqual(res.minValue, f.expected.minValue);
       assert.deepEqual(res.maxValue, f.expected.maxValue);
-      assert.deepEqual(res.message.toString("hex"), f.expected.message);
-      assert.deepEqual(res.blindFactor.toString("hex"), f.expected.blindFactor);
+      assert.deepEqual(res.message.toString('hex'), f.expected.message);
+      assert.deepEqual(res.blindFactor.toString('hex'), f.expected.blindFactor);
     });
   });
 });
